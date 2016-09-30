d209d7461f12e6b8e40eba5192cd3da34a87ab8e
DVxa <dv_rus@bk.ru>
2016-09-30 23:19:36 +0700

    30.09.16 5

--- src/components/FAQ/TabsFAQ.jsx
+++ src/components/FAQ/TabsFAQ.jsx
@@ -1,4 +1,71 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
 
+const styles = {
+    headline: {
+        fontSize: 24,
+        paddingTop: 16,
+        marginBottom: 12,
+        fontWeight: 400,
+    },
+};
 
+export default class TabsFAQ extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '2',
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange (value) {
+        this.setState({
+            value: value
+        });
+        console.log(value);
+    };
+
+    render() {
+        return (
+            <Tabs
+                value={this.state.value}
+                onChange={this.handleChange}
+            >
+                <Tab label="Все" value="1">
+                    <div>
+                        <h2>Все</h2>
+                        <p>
+                            This is an example tab.
+                        </p>
+                    </div>
+                </Tab>
+                <Tab label="О сервисе" value="2">
+                    <div>
+                        <h2>О сервисе</h2>
+                        <p>
+                            This is an example tab.
+                        </p>
+                    </div>
+                </Tab>
+                <Tab label="Заемщикам" value="3" >
+                    <div>
+                        <h2>Заемщикам</h2>
+                        <p>
+                            This is an example tab.
+                        </p>
+                    </div>
+                </Tab>
+                <Tab label="Кредиторам" value="4">
+                    <div>
+                        <h2>Кредиторам</h2>
+                        <p>
+                            This is another example tab.
+                        </p>
+                    </div>
+                </Tab>
+            </Tabs>
+        )
+    }
+}
